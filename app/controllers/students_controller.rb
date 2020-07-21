6b4d0deac5a17fe945f9dc65a08edfe45c8c0dd5
Ben Garlock <evilbunnyrabbits@gmail.com>
2020-07-21 18:25:57 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,4 +4,35 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def show
+    @student = Student.find(params[:id])
+    @ducks = Duck.all
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    student = Student.create(student_params)
+    redirect_to student_path(student)
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    student = Student.find(params[:id])
+    student.update(student_params)
+    redirect_to student_path
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name, :mod)
+  end
+
+
 end
