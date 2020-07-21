2323340e8854d4929b3f30d8fc26e0435b05d4ce
elandau91 <eli.b.landau@gmail.com>
2020-07-21 17:37:24 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
+
+Student.destroy_all
+Duck.destroy_all
+
+
 10.times do
   Student.create(name: Faker::Name.name, mod: rand(1..5))
 end
 
-5.times do
-  Duck.create(name: Faker::Creature::Dog.meme_phrase, description: Faker::Creature::Dog.breed)
+20.times do
+  Duck.create(name: Faker::Creature::Dog.meme_phrase, description: Faker::Creature::Dog.breed, student_id: Student.all.sample.id)
 end
+
+puts "completed!"
