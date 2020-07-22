1d61831a6349db066b67957306707a15b2252b33
yulia <juliana.ny2008@gmail.com>
2020-07-22 13:04:27 -0400

    Done.

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -1,7 +1,39 @@
 class DucksController < ApplicationController
+    before_action :find_duck, only: [:show, :edit, :update]
 
   def index
     @ducks = Duck.all
   end
+
+  def show 
+  end 
+
+  def new 
+@duck=Duck.new
+  end 
+
+  def edit 
+    
+  end 
+
+  def update 
+@duck.update(duck_params)
+redirect_to duck_path(@duck)
+  end 
+
+  def create 
+@duck=Duck.create(duck_params)
+redirect_to duck_path(@duck)
+  end
+  private 
+
+  def duck_params 
+    params.require(:duck).permit(:name, :description, :student_id)
+  end
+
+  def find_duck 
+    @duck=Duck.find(params[:id]) 
+  end
+
 
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,50 @@
 class StudentsController < ApplicationController
 
+  before_action :find_student, only: [:show, :edit, :update]
   def index
     @students = Student.all
+  end
+
+  def show 
+  end 
+
+  def new 
+    @mod=[1,2,3,4,5]
+  @student=Student.new
+  end 
+
+  def edit 
+  end 
+
+  def update 
+@student.update(student_params)
+if @student.valid? 
+  redirect_to student_path(@student)
+ else 
+   flash[:my_errors] = @student.errors.full_messages
+   redirect_to edit_student_path
+   end
+  end 
+
+  def create 
+ @student=Student.create(student_params)
+if @student.valid? 
+ redirect_to student_path(@student)
+else 
+  flash[:my_errors] = @student.errors.full_messages
+      
+      redirect_to new_student_path
+  end
+end
+
+  private 
+
+  def student_params 
+    params.require(:student).permit(:name, :mod)
+  end
+
+  def find_student 
+    @student=Student.find(params[:id]) 
   end
 
 end
