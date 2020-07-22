4468c169090c72ba95919ead5ecfceda6819bb72
jakemeout <jacobosity@gmail.com>
2020-07-22 08:14:22 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,8 @@
 class Student < ApplicationRecord
+    has_many :ducks
+
+    validates :name, presence: true
+    validates :name, uniqueness: true
+    validates :mod, numericality: {only_integer: true}
+    validates :mod, numericality: {less_than: 5}
 end
