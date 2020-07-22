65cf96b7e6f718a16a78834e20a127e6add0c0b8
Michael Horowitz <mjhorowitz714@yahoo.com>
2020-07-22 13:09:13 -0400

    Done.

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -1,7 +1,50 @@
 class DucksController < ApplicationController
 
+  before_action :find_duck, only: [:show, :edit, :update]
+
   def index
     @ducks = Duck.all
+    render :index
   end
+
+  def show
+    #@duck = Duck.find(params[:id])
+    render :show
+  end
+
+  def new
+    @duck = Duck.new
+    render :new
+  end
+
+  def create
+   duck = Duck.create(duck_params)
+
+   redirect_to duck_path(duck)
+  end
+
+  def edit
+   # @duck = Duck.find(params[:id])
+
+  end
+
+  def update
+  #  @duck = Duck.find(params[:id])
+
+    @duck.update(duck_params)
+
+    redirect_to duck_path(@duck)
+  end
+
+  private
+
+  def duck_params
+    params.require(:duck).permit(:name, :description)
+  end
+
+  def find_duck
+    @duck = Duck.find(params[:id])
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,25 @@
 Rails.application.routes.draw do
-  resources :ducks, only: [:index]
-  resources :students, only: [:index]
+  resources :ducks, only: [:index, :show, :new, :create, :update, :edit]
+  resources :students, only: [:index, :show, :new, :create, :update, :edit]
+  
+  # resources :ducks, except: [:destroy]
+  # resources :students, except: [:destroy]
+
+  # get "/ducks", to: "/ducks#index", as: 'ducks'
+  # get "/ducks/new", to: "/ducks#new", as: "new_duck"
+  # get "/ducks/:id", to: "/ducks#show", as: "duck"
+  # get "/ducks/:id/edit", to: "/ducks#edit", as: "edit_duck"
+  
+  # post "/ducks", to: "/ducks#create"
+  # patch "/ducks/:id", to: "/ducks#update"
+  # destroy "/ducks/:id", to: "/ducks#destroy"
+
+  # get "/students", to: "/students#index", as: "students"
+  # get "/students/new", to: "/students#new", as: "new_student"
+  # get "/students/:id", to: "/students#show", as: "student"
+  # get "/students/:id/edit", to: "/students#edit", as: "edit_student"
+ 
+  # post "/students", to: "/students#create"
+  # patch "/students/:id", to: "/students#update"
+  # destroy "/students/:id", to: "/students#destroy"
 end
