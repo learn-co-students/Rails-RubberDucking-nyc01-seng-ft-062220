d57090bdc4758bac83380b0ea7a903bde2c001a6
Luis Reyes <luisreyesxv@yahoo.com>
2020-07-21 23:14:50 -0400

    Done.

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -4,4 +4,45 @@ class DucksController < ApplicationController
     @ducks = Duck.all
   end
 
+  def new
+    @duck = Duck.new
+    @students = Student.all
+  end
+
+  def create
+    @duck = Duck.create(ducks_params)
+
+    if @duck.valid?
+      redirect_to duck_path(@duck)
+    else
+      flash[:my_errors] = @duck.errors.full_messages
+      redirect_to new_duck_path
+    end
+  end
+
+  def show
+    @duck = Duck.find(params[:id])
+  end
+
+  def edit
+    @duck= Duck.find(params[:id])
+    @students = Student.all
+  end
+
+  def update
+    @duck = Duck.new(ducks_params)
+    if @duck.save
+      redirect_to duck_path(@duck)
+    else
+      flash[:my_errors] = @duck.errors.full_messages
+      redirect_to edit_duck_path
+    end
+  end
+
+  private
+  def ducks_params
+    params.require(:duck).permit(:name,:type,:student_id)
+  end
+
+
 end
--- app/models/duck.rb
+++ app/models/duck.rb
@@ -1,2 +1,10 @@
 class Duck < ApplicationRecord
+    belongs_to :student
+
+    validates :student_id, :name, presence: true
+
+    def student_name
+        self.student.name
+    end
+    
 end
