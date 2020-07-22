04602f23ee1f7671258a83f2694176cef9c54ff0
kylejb <47502769+kylejb@users.noreply.github.com>
2020-07-21 23:08:53 -0400

    Assignment Deliverables

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -1,7 +1,55 @@
 class DucksController < ApplicationController
 
+  before_action :find_duck, except: [:index, :new, :create]
+
   def index
     @ducks = Duck.all
+  end
+
+  def show
+  end
+
+  def new
+    @duck = Duck.new
+  end
+
+  def edit
+  end
+
+
+  def create
+    byebug
+    @duck = Duck.new(duck_params)
+    
+    if @duck.save
+      redirect_to duck_path(@duck)
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @duck.update(duck_params)
+      redirect_to duck_path(@duck)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @duck.destroy
+
+    redirect_to ducks_path
+  end
+
+  private
+
+  def duck_params
+    params.require(:duck).permit(:name, :description, :student)
+  end
+
+  def find_duck
+    @duck = Duck.find(params[:id])
   end
 
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,55 @@
 class StudentsController < ApplicationController
 
+  before_action :find_student, except: [:index, :new, :create]
+
   def index
     @students = Student.all
+  end
+
+  def show
+  end
+
+  def new
+    @student = Student.new
+  end
+  
+  def edit
+  end
+
+  def create
+    if @student.create(student_params)
+      redirect_to student_path(@student)
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @student.update(student_params)
+      redirect_to student_path(@student)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @student.destroy
+
+    redirect_to students_path
+  end
+  
+
+
+
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name, :mod)
+  end
+
+  def find_student
+    @student = Student.find(params[:id])
   end
 
 end
