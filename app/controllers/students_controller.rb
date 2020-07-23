8d6ca718bbe8e1bb5d69c2415f5de05844c23c37
Aiannn <soktoevayan@gmail.com>
2020-07-22 20:24:57 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,4 +4,35 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new 
+  end
+
+  def create
+    @student = Student.new(s_params)
+    @student.save 
+    redirect_to student_path(@student)
+  end
+
+  def edit 
+    @student = Student.find(params[:id])
+  end 
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(s_params)
+    redirect_to student_path(@student)
+  end
+
+
+  private
+
+  def s_params
+    params.require(:student).permit(:name, :mod)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
+Student.destroy_all
+Duck.destroy_all
+
+a=[ 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 ]
+
 10.times do
   Student.create(name: Faker::Name.name, mod: rand(1..5))
 end
 
 5.times do
-  Duck.create(name: Faker::Creature::Dog.meme_phrase, description: Faker::Creature::Dog.breed)
+  Duck.create(name: Faker::Creature::Dog.meme_phrase, description: Faker::Creature::Dog.breed, student_id: a.sample)
 end
